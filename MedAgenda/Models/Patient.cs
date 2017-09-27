4a5f67e9ed0f840d6723ce4fa9b3de7dc081f56e
Brittany LaCoste <lacostebrittany0227@gmail.com>
2017-09-27 16:58:48 -0700

    Patient model created

---
+++ MedAgenda/Models/Patient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedAgenda
+{
+    public class Patient
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public string MobilePhone { get; set; }
+        public string HomePhone { get; set; }
+        public DateTime DOB { get; set; }
+        public string ConsentForCare{ get; set; }
+        public string ConsentForText { get; set; }
+    }
+}
