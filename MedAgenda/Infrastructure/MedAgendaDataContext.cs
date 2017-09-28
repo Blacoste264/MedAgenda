1722ea4faf04cdd8d18fc1e7ba54c903c3845d3b
Brittany LaCoste <lacostebrittany0227@gmail.com>
2017-09-27 17:56:08 -0700

    New patient model and DBcontext

---
+++ MedAgenda/Infrastructure/MedAgendaDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Web;
+using MedAgenda.Models;
+
+namespace MedAgenda.Infrastructure
+{
+    public class MedAgendaDataContext : DbContext
+    {
+        public MedAgendaDataContext() : base("MedAgenda")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MedAgenda.Models.Patient> Patients { get; set; }
+    }
+}
--- MedAgenda/Models/Patient.cs
+++ MedAgenda/Models/Patient.cs
@@ -1,12 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace MedAgenda
+namespace MedAgenda.Models
 {
     public class Patient
     {
+        public int PatientId { get; set; }
+
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
@@ -18,7 +19,9 @@ namespace MedAgenda
         public string MobilePhone { get; set; }
         public string HomePhone { get; set; }
         public DateTime DOB { get; set; }
-        public string ConsentForCare{ get; set; }
+        public string ConsentForCare { get; set; }
         public string ConsentForText { get; set; }
+
     }
 }
+    
